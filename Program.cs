ab55ada5feed8ead0d0fc5af497ae272686695c5
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-05-16 10:53:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio_1_y_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Ejercicio 1 del Capitulo 1.
+            Console.Write("1- Cree un proyecto nuevo que imprima su nombre en la ventana de la consola.\n\n");
+            Console.Write("Eladio Fermin Fermin\n\n");
+
+            //Ejercicio 5 del  Capitulo 1.
+            Console.Write("Agregue más mensajes a la aplicación que ha creado.\n\n");
+            Console.Write("Hola ");
+            Console.Write("mi nombre es ");
+            Console.Write("Eladio Fermin Fermin\n\n");
+        }
+    }
+}
